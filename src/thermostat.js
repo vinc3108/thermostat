791e8b021cb407a6c17df8110569c9d3c0f7d653
yy140 <74052695+yy140@users.noreply.github.com>
2021-02-24 12:31:39 +0000

    index.html added

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,11 +1,14 @@
 'use strict';
 class Thermostat{
   constructor() {
+    this.DEFAULT_TEMPERATURE = 20;
     this.MINIMUM_TEMPERATURE = 10;
-    this.temperature = 20;
+    this.temperature = this.DEFAULT_TEMPERATURE;
     this.powerSavingMode = true;
     this.MAX_LIMIT_PSM_ON = 25;
     this.MAX_LIMIT_PSM_OFF = 32;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+    this.HIGH_ENERGY_USAGE_LIMIT = 25;
   }
   getCurrentTemperature() {
     return this.temperature;
@@ -27,6 +30,18 @@ class Thermostat{
   }
   switchPowerSavingModeOn() {
     this.powerSavingMode = true;
+  }
+  resetTemperature() {
+    this.temperature = this.DEFAULT_TEMPERATURE;
+  }
+  energyUsage() {
+    if(this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return 'low-usage';
+    } else if(this.temperature <= this.HIGH_ENERGY_USAGE_LIMIT) {
+      return 'medium-usage';    
+    } else {
+      return 'high-usage';
+    }
   }
   isMiniumTemperature() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
